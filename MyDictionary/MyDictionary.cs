78608c8380f1f59d8a63b301737d6560850ad42e
Aydemir Özcan <aydemir_kanarya@hotmail.com>
2021-01-23 20:51:12 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T,A>
+    {
+        T[] Keys;
+        A[] Values;
+        public MyDictionary()
+        {
+            Keys = new T[0];
+            Values = new A[0];
+        }
+
+        public void Add(T Key, A Value)
+        {
+            T[] tempArray1 = Keys;
+            A[] tempArray2 = Values;
+
+            Keys = new T[Keys.Length + 1];
+            Values = new A[Values.Length + 1];
+
+            for (int i = 0; i < tempArray1.Length; i++)
+            {
+                Keys[i] = tempArray1[i];
+            }
+
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                Values[i] = tempArray2[i];
+            }
+
+
+            Keys[Keys.Length - 1] = Key;
+            Values[Values.Length - 1] = Value;
+        }
+
+
+           public int CountKeys
+        {
+            get { return Keys.Length; }
+        }
+
+
+        public int CountValues
+        {
+            get { return Values.Length; }
+        }
+    
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();  //string : runner , int: time (m/min)
+            myDictionary.Add("Aydemir", 30);
+            myDictionary.Add("UsainBolt", 70);
+            myDictionary.Add("Powel", 60);
+
+            Console.WriteLine(myDictionary.CountKeys);
+            Console.WriteLine(myDictionary.CountValues);
+
+          
+
+        }
+    }
+}
